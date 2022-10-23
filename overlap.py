189a86a956edf45c60d117cb1fde2d93b79ed6f5
Mete Ismayil <mismayilza@gmail.com>
2022-10-23 13:03:07 +0200

    updates

--- overlap.py
+++ overlap.py
@@ -1,43 +1,55 @@
 from tqdm import tqdm
 import pandas as pd
 import re
+import argparse
+import pathlib
 
 from helpers import has_overlap, has_overlap_with_story
 
-OVERLAP_THRESHOLD = 0.9
-dim_overlaps = [[], [], [], [], [], [], [], [], [], []]
-dim_story_overlaps = [[], [], [], [], [], [], [], [], [], []]
+OVERLAP_THRESHOLD = 0.1
 
-glucose = pd.read_csv("data/GLUCOSE_training_data_final.csv")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
 
-for i, row in tqdm(glucose.iterrows(), total=len(glucose)):
+    parser.add_argument("--dataset", type=str, help="Path to dataset")
+    parser.add_argument("--threshold", type=float, default=OVERLAP_THRESHOLD, help="Overlap threshold")
+
+    args = parser.parse_args()
+
+    dim_overlaps = [[], [], [], [], [], [], [], [], [], []]
+    dim_story_overlaps = [[], [], [], [], [], [], [], [], [], []]
+
+    data = pd.read_csv(args.dataset)
+
+    for i, row in tqdm(data.iterrows(), total=len(data)):
+        for dim in range(1, 11):
+            specific_rule = row[f"{dim}_specificNL"].strip()
+            general_rule = row[f"{dim}_generalNL"].strip()
+
+            if specific_rule == "escaped" or general_rule == "escaped":
+                dim_overlaps[dim-1].append([None, None])
+                dim_story_overlaps[dim-1].append(None)
+            else:
+                specific_parts = re.split(">.*>", specific_rule)
+                general_parts = re.split(">.*>", general_rule)
+
+                assert len(specific_parts) == len(general_parts) == 2
+
+                overlap = []
+
+                for sp_text, gl_text in zip(specific_parts, general_parts):
+                    overlap.append(has_overlap(sp_text.strip(), gl_text.strip(), args.threshold))
+
+                dim_overlaps[dim-1].append(overlap)
+
+                story_overlap = has_overlap_with_story(specific_parts[1].strip(), row['story'], args.threshold)
+                dim_story_overlaps[dim-1].append(story_overlap)
+
+    dim_overlap_df = pd.DataFrame()
+
     for dim in range(1, 11):
-        specific_rule = row[f"{dim}_specificNL"].strip()
-        general_rule = row[f"{dim}_generalNL"].strip()
+        dim_overlap_df[f"{dim}_has_overlap_{args.threshold}"] = dim_overlaps[dim-1]
+        dim_overlap_df[f"{dim}_has_story_overlap_{args.threshold}"] = dim_story_overlaps[dim-1]
 
-        if specific_rule == "escaped" or general_rule == "escaped":
-            dim_overlaps[dim-1].append([None, None])
-            dim_story_overlaps[dim-1].append(None)
-        else:
-            specific_parts = re.split(">.*>", specific_rule)
-            general_parts = re.split(">.*>", general_rule)
-
-            assert len(specific_parts) == len(general_parts) == 2
-
-            overlap = []
-
-            for sp_text, gl_text in zip(specific_parts, general_parts):
-                overlap.append(has_overlap(sp_text.strip(), gl_text.strip(), OVERLAP_THRESHOLD))
-
-            dim_overlaps[dim-1].append(overlap)
-
-            story_overlap = has_overlap_with_story(specific_parts[1].strip(), row['story'], OVERLAP_THRESHOLD)
-            dim_story_overlaps[dim-1].append(story_overlap)
-
-dim_overlap_df = pd.DataFrame()
-
-for dim in range(1, 11):
-    dim_overlap_df[f"{dim}_has_overlap_{OVERLAP_THRESHOLD}"] = dim_overlaps[dim-1]
-    dim_overlap_df[f"{dim}_has_story_overlap_{OVERLAP_THRESHOLD}"] = dim_story_overlaps[dim-1]
-
-dim_overlap_df.to_csv("glucose_overlap.csv")
+    data_with_overlap = pd.concat([data, dim_overlap_df], axis=1)
+    data_with_overlap.to_csv(f"{pathlib.Path(args.dataset).stem}_with_overlap_{args.threshold}.csv")
