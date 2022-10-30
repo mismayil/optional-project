4b07c008597ac714708b0f6668bc7f30316dae29
Mete Ismayil <mismayilza@gmail.com>
2022-10-30 20:21:59 +0100

    add new story overlap results

--- overlap.py
+++ overlap.py
@@ -4,69 +4,114 @@ import re
 import argparse
 import pathlib
 
-from utils import has_overlap, has_overlap_with_story
+from utils import has_rule_overlap, has_story_overlap, has_story_copy
 
 OVERLAP_THRESHOLD = 0.1
+RULE_OVERLAP = "rule"
+STORY_OVERLAP = "story"
+STORY_COPY_OVERLAP = "copy"
 
 def group_overlap_by_dim(data, dim=1, threshold=0.0):
     non_escaped = data[data[f"{dim}_specificNL"] != "escaped"]
-    spec_gen_col = f"{dim}_has_overlap_{threshold}"
-    spec_story_col = f"{dim}_has_story_overlap_{threshold}"
-    samples_by_overlap = non_escaped.groupby([spec_gen_col, spec_story_col]).experiment_id.count()
-    return samples_by_overlap.rename_axis(index={spec_gen_col: "spec_gen_overlap", spec_story_col: "spec_story_overlap"}).rename(f"dim{dim}")
+    rule_overlap_col = f"{dim}_rule_overlap_{threshold}"
+    story_overlap_col = f"{dim}_story_overlap_{threshold}"
+    story_copy_overlap_col = f"{dim}_story_copy_overlap_{threshold}"
+    group_cols = []
+
+    if rule_overlap_col in data.columns:
+        group_cols.append(rule_overlap_col)
+    
+    if story_overlap_col in data.columns:
+        group_cols.append(story_overlap_col)
+
+    if story_copy_overlap_col in data.columns:
+        group_cols.append(story_copy_overlap_col)
+
+    samples_by_overlap = non_escaped.groupby(group_cols).experiment_id.count()
+    return samples_by_overlap.rename_axis(index={rule_overlap_col: "rule_overlap", story_overlap_col: "story_overlap", story_copy_overlap_col: "story_copy"}).rename(f"dim{dim}")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument("--dataset", type=str, help="Path to dataset")
     parser.add_argument("--threshold", type=float, default=OVERLAP_THRESHOLD, help="Overlap threshold")
+    parser.add_argument("--dims", type=int, nargs="+", default=list(range(1, 11)), help="Which dimensions to run the overlap for.")
+    parser.add_argument("--dim-based-overlap", action="store_true", default=False, help="Whether to consider overlap with story based on dimension. e.g. for dim1 consider previous sentence")
+    parser.add_argument("--overlap-types", type=str, nargs="+", choices=[RULE_OVERLAP, STORY_OVERLAP, STORY_COPY_OVERLAP], default=[RULE_OVERLAP, STORY_OVERLAP], help="What types of overlap to consider.")
 
     args = parser.parse_args()
 
-    dim_overlaps = [[], [], [], [], [], [], [], [], [], []]
+    dim_rule_overlaps = [[], [], [], [], [], [], [], [], [], []]
     dim_story_overlaps = [[], [], [], [], [], [], [], [], [], []]
+    dim_story_copy_overlaps = [[], [], [], [], [], [], [], [], [], []]
 
     data = pd.read_csv(args.dataset)
 
     for i, row in tqdm(data.iterrows(), total=len(data)):
-        for dim in range(1, 11):
+        for dim in args.dims:
             specific_rule = row[f"{dim}_specificNL"].strip()
             general_rule = row[f"{dim}_generalNL"].strip()
 
             if specific_rule == "escaped" or general_rule == "escaped":
-                dim_overlaps[dim-1].append([None, None])
-                dim_story_overlaps[dim-1].append(None)
+                dim_rule_overlaps[dim-1].append([None, None])
+                dim_story_overlaps[dim-1].append([None, None])
+                dim_story_copy_overlaps[dim-1].append([None, None, None, None])
             else:
                 specific_parts = re.split(">.*>", specific_rule)
                 general_parts = re.split(">.*>", general_rule)
 
                 assert len(specific_parts) == len(general_parts) == 2
 
-                overlap = []
+                if RULE_OVERLAP in args.overlap_types:
+                    rule_overlap = []
 
-                for sp_text, gl_text in zip(specific_parts, general_parts):
-                    overlap.append(has_overlap(sp_text.strip(), gl_text.strip(), args.threshold))
+                    for sp_text, gl_text in zip(specific_parts, general_parts):
+                        rule_overlap.append(has_rule_overlap(sp_text.strip(), gl_text.strip(), args.threshold))
 
-                dim_overlaps[dim-1].append(overlap)
+                    dim_rule_overlaps[dim-1].append(rule_overlap)
 
-                story_overlap = has_overlap_with_story(specific_parts[1].strip(), row['story'], args.threshold)
-                dim_story_overlaps[dim-1].append(story_overlap)
+                if STORY_OVERLAP in args.overlap_types:
+                    specific_story_overlap = has_story_overlap(specific_parts[1].strip(), row['story'],
+                                                                threshold=args.threshold,
+                                                                dim_based_overlap=args.dim_based_overlap,
+                                                                selected_context=row["selected_sentence"],
+                                                                dim=dim)
+                    general_story_overlap = has_story_overlap(general_parts[1].strip(), row['story'],
+                                                                threshold=args.threshold,
+                                                                dim_based_overlap=args.dim_based_overlap,
+                                                                selected_context=row["selected_sentence"],
+                                                                dim=dim)
+                    dim_story_overlaps[dim-1].append([specific_story_overlap, general_story_overlap])
+                
+                if STORY_COPY_OVERLAP in args.overlap_types:
+                    sp_copy_overlap, sp_copy_index, sp_copy_mode = has_story_copy(specific_parts[0].strip(), specific_parts[1].strip(), row["story"], threshold=args.threshold)
+                    gl_copy_overlap, gl_copy_index, gl_copy_mode = has_story_copy(general_parts[0].strip(), general_parts[1].strip(), row["story"], threshold=args.threshold)
+                    dim_story_copy_overlaps[dim-1].append([sp_copy_overlap, sp_copy_mode, gl_copy_overlap, gl_copy_mode])
 
     dim_overlap_df = pd.DataFrame()
 
-    for dim in range(1, 11):
-        dim_overlap_df[f"{dim}_has_overlap_{args.threshold}"] = dim_overlaps[dim-1]
-        dim_overlap_df[f"{dim}_has_story_overlap_{args.threshold}"] = dim_story_overlaps[dim-1]
+    for dim in args.dims:
+        if RULE_OVERLAP in args.overlap_types:
+            dim_overlap_df[f"{dim}_rule_overlap_{args.threshold}"] = dim_rule_overlaps[dim-1]
+        if STORY_OVERLAP in args.overlap_types:
+            dim_overlap_df[f"{dim}_story_overlap_{args.threshold}"] = dim_story_overlaps[dim-1]
+        if STORY_COPY_OVERLAP in args.overlap_types:
+            dim_overlap_df[f"{dim}_story_copy_overlap_{args.threshold}"] = dim_story_copy_overlaps[dim-1]
+
+    dims_text = "all" if len(args.dims) == 10 else "".join([str(d) for d in args.dims])
+    dbo_text = "_dbo" if args.dim_based_overlap else ""
+    overlap_type_text = "".join([otype[0] for otype in args.overlap_types])
+    suffix = f"overlap_{args.threshold}_dim_{dims_text}_{overlap_type_text}{dbo_text}"
 
     data_with_overlap = pd.concat([data, dim_overlap_df], axis=1)
-    with_overlap_file = f"outputs/{pathlib.Path(args.dataset).stem}_with_overlap_{args.threshold}.csv"
+    with_overlap_file = f"outputs/{pathlib.Path(args.dataset).stem}_with_{suffix}.csv"
     data_with_overlap.to_csv(with_overlap_file)
 
     data_with_overlap = pd.read_csv(with_overlap_file)
-    dim_overlap_stats_df = group_overlap_by_dim(data_with_overlap, dim=1, threshold=args.threshold)
+    dim_overlap_stats_df = group_overlap_by_dim(data_with_overlap, dim=args.dims[0], threshold=args.threshold)
 
-    for dim in range(2, 11):
+    for dim in args.dims[1:]:
         dim_overlap_stats_df = pd.merge(dim_overlap_stats_df, group_overlap_by_dim(data_with_overlap, dim=dim, threshold=args.threshold), left_index=True, right_index=True, how="outer")
     
     dim_overlap_stats_df = dim_overlap_stats_df.reset_index().fillna(0)
-    dim_overlap_stats_df.to_csv(f"outputs/{pathlib.Path(args.dataset).stem}_overlap_{args.threshold}_stats.csv")
+    dim_overlap_stats_df.to_csv(f"outputs/{pathlib.Path(args.dataset).stem}_stats_{suffix}.csv")
--- utils.py
+++ utils.py
@@ -1,50 +1,109 @@
+from typing import List
 import spacy
 import torch
 from torch.utils.data import Dataset
 import pandas as pd
+import re
 
-nlp = spacy.load("en_core_web_sm", exclude=["ner", "parser"])
+nlp = spacy.load("en_core_web_sm")
 
 doc_cache = {}
+sent_cache = {}
 
 def get_doc(text):
-    if text in doc_cache:
-        return doc_cache[text]
-    
-    doc = nlp(text)
-    doc_cache[text] = doc
+    if text not in doc_cache:
+        doc = nlp(text)
+        doc_cache[text] = doc
 
-    return doc
+    return doc_cache[text]
+
+def get_sents(text):
+    if text not in sent_cache:
+        doc = nlp(text)
+        sent_cache[text] = [str(sent) for sent in doc.sents]
+
+    return sent_cache[text]
 
 with open("outputs/placeholders.txt") as f:
     placeholders = f.read().splitlines()
 
-def has_overlap(specific_text: str, general_text: str, threshold: float) -> bool:
-    sp_doc = get_doc(specific_text)
-    gl_doc = get_doc(general_text)
+def get_words(text: str, ignore_stopwords: bool = True, ignore_entities: bool = True):
+    doc = get_doc(text)
+    tokens = [token for token in doc if token.text not in placeholders and not token.is_punct and len(token.text.strip()) > 0]
+    
+    if ignore_stopwords:
+        tokens = [token for token in tokens if not token.is_stop]
 
-    sp_words = set([token.lemma_.lower() for token in sp_doc])
-    gl_words = set([token.lemma_.lower() for token in gl_doc if token.text not in placeholders])
+    if ignore_entities:
+        ents = [ent.text for ent in doc.ents]
+        tokens = [token for token in tokens if token.text not in ents]
+
+    return set([token.lemma_.lower() for token in tokens])
+
+def has_rule_overlap(specific_text: str, general_text: str, threshold: float) -> bool:
+    sp_words = get_words(specific_text)
+    gl_words = get_words(general_text)
 
     if len(gl_words) > 0:
         return (len(sp_words.intersection(gl_words)) / len(gl_words)) > threshold
     
     return False
 
+def has_context_overlap(words: List[str], context: str, threshold: float, ignore_stopwords: bool = True, ignore_entities: bool = True):
+    context_words = get_words(context, ignore_stopwords=ignore_stopwords, ignore_entities=ignore_entities)
 
-def has_overlap_with_story(text: str, story: str, threshold: float) -> bool:
-    contexts = story.split(".")
-    doc = get_doc(text)
-    words = set([token.lemma_.lower() for token in doc])
+    if len(words) > 0 and (len(words.intersection(context_words)) / len(words)) > threshold:
+        return True
+    
+    return False
 
-    for context in contexts:
-        context_doc = get_doc(context)
-        context_words = set([token.lemma_.lower() for token in context_doc])
+def has_story_overlap(text: str, story: str, threshold: float, dim_based_overlap: bool = False, selected_context: str = None, dim: int = 1) -> bool:
+    text_words = get_words(text)
 
-        if len(words) > 0 and (len(words.intersection(context_words)) / len(words)) > threshold:
-            return True
+    if dim_based_overlap and dim in [1, 6]:
+        story_parts = re.split(re.escape(selected_context), story)
+        past_context = story_parts[0]
+        future_context = story_parts[1]
+        context = None
+
+        if dim == 1 and past_context:
+            past_sents = get_sents(past_context)
+            context = past_sents[-1]
+        elif dim == 6 and future_context:
+            future_sents = get_sents(future_context)
+            context = future_sents[0]
+
+        if context is not None:
+            context_overlap = has_context_overlap(text_words, context, threshold=threshold)
+            return context_overlap
+    else:
+        contexts = get_sents(story)
+        for context in contexts:
+            context_overlap = has_context_overlap(text_words, context, threshold=threshold)
+            if context_overlap:
+                return True
 
     return False
+
+def has_story_copy(head: str, tail: str, story: str, threshold: float) -> bool:
+    head_words = get_words(head, ignore_stopwords=False, ignore_entities=False)
+    tail_words = get_words(tail, ignore_stopwords=False, ignore_entities=False)
+    sentences = get_sents(story)
+
+    for i, sentence in enumerate(sentences):
+        head_overlap = has_context_overlap(head_words, sentence, threshold, ignore_stopwords=False, ignore_entities=False)
+        if head_overlap:
+            if i > 0:
+                tail_overlap = has_context_overlap(tail_words, sentences[i-1], threshold, ignore_stopwords=False, ignore_entities=False)
+                if tail_overlap:
+                    return True, i, "backward"
+            if i < len(sentences)-1:
+                tail_overlap = has_context_overlap(tail_words, sentences[i+1], threshold, ignore_stopwords=False, ignore_entities=False)
+                if tail_overlap:
+                    return True, i, "forward"
+    
+    return False, 0, "none"
+
 
 def get_glucose_general_data(glucose_data):
     glucose_gen_data = pd.DataFrame()
