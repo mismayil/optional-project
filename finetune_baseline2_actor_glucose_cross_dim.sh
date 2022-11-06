4efe2f1975cb19ffdbbb7d4a950f65d5ad1563e5
Mete Ismayil <mismayilza@gmail.com>
2022-11-06 21:38:24 +0100

    add baseline2 finetune script

---
+++ finetune_baseline2_actor_glucose_cross_dim.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+${CONDA} run -n op --no-capture-output \
+    python t5_experiments/scripts/train_predict.py \
+    --training-file data/glucose_cross_dim_train_data.json \
+    --validation-file data/glucose_cross_dim_val_data.json \
+    --language-model /scratch/mete/op_baseline2_actor_pretrained_glucose_cross_dim \
+    --model-dir /scratch/mete/op_baseline2_actor_glucose_cross_dim \
+    --epochs 10 \
+    --batch-size 32 \
+    --val-batch-size 32 \
+    --input-label model_input \
+    --output-label model_output \
+    --wandb-run-name baseline2_actor_glucose_cross_dim
