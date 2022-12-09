365af54c2703a98a2309bc4092f8589c11de5088
Mete Ismayil <mismayilza@gmail.com>
2022-12-09 16:26:39 +0100

    bug fixes

--- evaluate_baseline_actor_moral_stories.sh
+++ evaluate_baseline_actor_moral_stories.sh
@@ -3,7 +3,7 @@
 ${CONDA} run -n op --no-capture-output \
     python t5_experiments/scripts/train_predict.py \
     --validation-file ${1} \
-    --model-dir /scratch/mete/op_baseline_actor_moral_stories \
+    --model-dir /scratch/mete/op/op_baseline_actor_moral_stories \
     --val-batch-size 32 \
     --input-label actor_input \
     --output-label actor_output \
