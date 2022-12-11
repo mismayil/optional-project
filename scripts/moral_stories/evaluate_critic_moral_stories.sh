679489dc346354ae1764cccf17862ef8d9d16126
Mete Ismayil <mismayilza@gmail.com>
2022-12-11 17:30:41 +0100

    add critic evaluate script

---
+++ scripts/moral_stories/evaluate_critic_moral_stories.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+${CONDA} run -n op --no-capture-output \
+    python t5_experiments/scripts/train_predict.py \
+    --validation-file ${1} \
+    --model-dir /scratch/mete/op/op_critic_moral_stories \
+    --val-batch-size 16 \
+    --input-label critic_input \
+    --output-label critic_output \
+    --max-input-length 256 \
+    --max-output-length 60 \
+    --save-results outputs
