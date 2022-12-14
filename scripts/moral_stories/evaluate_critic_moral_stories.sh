44911b06f23549dc51fce5c6c573cf2c586bb317
Mete Ismayil <mismayilza@gmail.com>
2022-12-14 12:42:43 +0000

    add critic vp results

--- scripts/moral_stories/evaluate_critic_moral_stories.sh
+++ scripts/moral_stories/evaluate_critic_moral_stories.sh
@@ -3,10 +3,11 @@
 ${CONDA} run -n op --no-capture-output \
     python t5_experiments/scripts/train_predict.py \
     --validation-file ${1} \
-    --model-dir /scratch/mete/op/op_critic_moral_stories \
+    --model-dir /scratch/mete/op/op_critic_moral_stories_vp \
     --val-batch-size 16 \
     --input-label critic_input \
     --output-label critic_output \
     --max-input-length 256 \
     --max-output-length 60 \
     --save-results outputs
+
