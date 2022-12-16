4120abf231f1df07b3fceeb56608bfee671f757d
Mete Ismayil <mismayilza@gmail.com>
2022-12-16 01:52:18 +0100

    bug fixes

--- scripts/moral_stories/train_critic_moral_stories.sh
+++ scripts/moral_stories/train_critic_moral_stories.sh
@@ -2,16 +2,16 @@
 
 ${CONDA} run -n op --no-capture-output \
     python t5_experiments/scripts/train_predict.py \
-    --training-file data/moral_stories/norm-actions+context+consequences/norm_distance/critic_train_prep_vp_extended_with_norm_np_final.json \
-    --validation-file data/moral_stories/norm-actions+context+consequences/norm_distance/critic_dev_prep_vp_extended_with_norm_np_final.json \
+    --training-file data/moral_stories/norm-actions+context+consequences/norm_distance/critic_train_prep_final.json \
+    --validation-file data/moral_stories/norm-actions+context+consequences/norm_distance/critic_dev_prep_final.json \
     --language-model allenai/unifiedqa-t5-base \
-    --model-dir /scratch/mete/op/op_critic_moral_stories_norm_only_base \
+    --model-dir /scratch/mete/op/op_critic_moral_stories_cat \
     --epochs 10 \
-    --batch-size 16 \
-    --val-batch-size 16 \
+    --batch-size 32 \
+    --val-batch-size 32 \
     --input-label critic_input \
     --output-label critic_output \
     --max-input-length 256 \
     --max-output-length 60 \
-    --wandb-run-name critic_moral_stories_norm_only_base \
-    --save-results /scratch/mete/op/critic_moral_stories_norm_only_base_outputs
+    --wandb-run-name critic_moral_stories_cat \
+    --save-results /scratch/mete/op/critic_moral_stories_cat_outputs
