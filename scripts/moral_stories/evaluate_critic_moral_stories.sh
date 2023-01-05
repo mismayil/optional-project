0d4a6dca3759fbaf3eb5dd2de842890537f1c53d
Mete Ismayil <mismayilza@gmail.com>
2023-01-05 00:49:58 +0000

    add balanced critic test results

--- scripts/moral_stories/evaluate_critic_moral_stories.sh
+++ scripts/moral_stories/evaluate_critic_moral_stories.sh
@@ -3,7 +3,7 @@
 ${CONDA} run -n op --no-capture-output \
     python t5_experiments/scripts/train_predict.py \
     --validation-file ${1} \
-    --model-dir /scratch/mete/op/op_critic_moral_stories_vp \
+    --model-dir /scratch/mete/op/op_critic_moral_stories_balanced \
     --val-batch-size 16 \
     --input-label critic_input \
     --output-label critic_output \
