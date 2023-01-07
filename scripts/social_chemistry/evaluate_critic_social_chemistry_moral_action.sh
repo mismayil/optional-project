a3e53193d1951bb0c4fdd2241f06c614ec4184b0
Mete Ismayil <mismayilza@gmail.com>
2023-01-07 16:48:08 +0000

    add social chemistry eval scripts

---
+++ scripts/social_chemistry/evaluate_critic_social_chemistry_moral_action.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+${CONDA} run -n op --no-capture-output \
+    python t5_experiments/scripts/train_predict.py \
+    --validation-file ${1} \
+    --model-dir /scratch/mete/op/op_critic_social_chemistry \
+    --val-batch-size 64 \
+    --input-label moral_action \
+    --output-label norm \
+    --max-input-length 256 \
+    --max-output-length 60 \
+    --save-results data/moral_stories/norm-actions+context+consequences/norm_distance \
+    --prediction-label moral_social_norm
+
